199be52c34bab36a450fc106bb9e925afef9cd52
SBagaria2710 <shashwawtforwork@gmail.com>
2023-09-26 23:07:00 +0530

    Class 22

---
+++ class-22 (26-09-2023)/memory-of-object.js
@@ -0,0 +1,29 @@
+// Primitive DataTypes
+// let person1 = "Adam";
+// let person2 = "Steve";
+
+// person2 = person1;
+// person1 = person2;
+
+// console.log(person1);
+// console.log(person2);
+
+// ------ WITH OBJECT ------
+// let person1 = {
+//   firstName: "Adam",
+//   age: 25,
+// }
+
+// let person2 = person1;
+// person2.firstName = "Shashwat";
+
+// console.log(person1);
+// console.log(person2);
+
+let firstPerson = "Adam";
+let secondPerson = "Steve";
+
+firstPerson = "Steve";
+
+console.log(firstPerson);
+console.log(secondPerson);
