31ed3be75f9264d41e3468bb09a3c16d8e489717
SBagaria2710 <shashwawtforwork@gmail.com>
2023-11-07 21:06:02 +0530

    Update Folder name

--- class-36/src/constant.js
+++
@@ -1,1 +0,0 @@
-export const PRODUCTS_URL = "https://fakestoreapi.com/products";
--- class-36/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import cart from './slices/cart'
-
-export const store = configureStore({
-  reducer: {
-    cart
-  },
-})
