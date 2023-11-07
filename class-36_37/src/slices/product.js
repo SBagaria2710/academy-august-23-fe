aa40ac2ffb605e9a0189c0162abc495c88d7e7fe
SBagaria2710 <shashwawtforwork@gmail.com>
2023-11-07 23:16:21 +0530

    createAsyncThunk

--- class-36_37/src/slices/product.js
+++ class-36_37/src/slices/product.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { PRODUCTS_URL } from "../constant";
 
@@ -14,31 +14,47 @@ const productSlice = createSlice({
     data: [],
     status: STATUS.LOADING
   },
-  reducers: {
-    setProducts(state, action) {
-      state.data = action.payload
-    },
-    setStatus(state, action) {
-      state.status = action.payload
-    }
+  // reducers: {
+  //   setProducts(state, action) {
+  //     state.data = action.payload
+  //   },
+  //   setStatus(state, action) {
+  //     state.status = action.payload
+  //   }
+  // }
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      state.status = STATUS.LOADING;
+    }).addCase(fetchProducts.fulfilled, (state, action) => {
+      state.data = action.payload.data;
+      state.status = STATUS.SUCCESS;
+    }).addCase(fetchProducts.rejected, (state) => {
+      state.status = STATUS.FAILED;
+    })
   }
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-export function fetchProducts() {
-  return async function fetchProductsThunk(dispatch) {
-    dispatch(setStatus(STATUS.LOADING));
+// export function fetchProducts() {
+//   return async function fetchProductsThunk(dispatch) {
+//     dispatch(setStatus(STATUS.LOADING));
 
-    try {
-      const res = await axios.get(PRODUCTS_URL);
-      dispatch(setProducts(res.data));
-      dispatch(setStatus(STATUS.SUCCESS));
-    } catch (err) {
-      console.log(err);
-      dispatch(setStatus(STATUS.FAILED));
-    }
-  }
-}
+//     try {
+//       const res = await axios.get(PRODUCTS_URL);
+//       dispatch(setProducts(res.data));
+//       dispatch(setStatus(STATUS.SUCCESS));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUS.FAILED));
+//     }
+//   }
+// }
+
+export const fetchProducts = createAsyncThunk('products', async () => {
+  const res = await axios.get(PRODUCTS_URL);
+  return res;
+});
 
