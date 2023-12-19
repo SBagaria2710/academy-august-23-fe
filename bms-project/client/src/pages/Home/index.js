fe55d85ade1559df7dd9675efa0e00575b2cdd2f
SBagaria2710 <shashwawtforwork@gmail.com>
2023-12-19 21:44:56 +0530

    User Home Page

--- bms-project/client/src/pages/Home/index.js
+++ bms-project/client/src/pages/Home/index.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Row, Col, Input, message } from "antd";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { GetAllMovies } from "../../apicalls/movies";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 
 function Home() {
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await GetAllMovies();
+      if (response.success) {
+        setMovies(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>This is Home Route</div>
-  )
+    <div>
+      <Input
+        style={{ width: "600px" }}
+        type="text"
+        className="search-input"
+        placeholder="Search for movies"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <h1 className="text-md uppercase mb-2">Currently showing Movies</h1>
+      <Row gutter={[20]} className="mt-2">
+        {movies
+          .filter((movie) =>
+            movie.title.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .map((movie) => (
+            <Col span={4}>
+              <div
+                style={{ maxWidth: "190px" }}
+                className="card flex flex-col gap-3 cursor-pointer"
+                onClick={() =>
+                  navigate(
+                    `/movie/${movie._id}?date=${moment().format("YYYY-MM-DD")}`
+                  )
+                }
+              >
+                <img src={movie.poster} alt="" height={200} width={190} />
+
+                <div className="flex justify-center p-1">
+                  <h1 className="text-md uppercase">{movie.title}</h1>
+                </div>
+              </div>
+            </Col>
+          ))}
+      </Row>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
